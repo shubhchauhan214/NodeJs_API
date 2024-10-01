f5848c13311d2769c224d38c9d6cbd23fe50e153
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-10-01 14:25:41 +0530

    get methods

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ const PORT = 8000;
 
 //Routes
 
-app.get("/users", (req, res) =>{
+app.get("/api/users", (req, res) =>{
     return res.json(users);
+});
+
+// app.get("/users", (req, res) => {
+//     const html = `
+//     <ul>
+//       ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+//     </ul>`;
+//     res.send(html);
+// });
+
+app.get("/api/users/:id", (req, res) =>{
+    const id = Number(req.params.id);                             //fetch id
+    const user = users.find((user)=> user.id ===id);              //find id from mock_data.json
+    return res.json(user);
 });
 
 
